f3f41a5dee36a60cb4ce2efeb026f05876853900
myupup123 <91126419+myupup123@users.noreply.github.com>
2022-03-13 13:45:32 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { ConfigType } from "./types/ConfigType";
 
 export const Config: ConfigType = {
-    siteName: "AHdark Status",
+    siteName: "Seanhui Status",
     apikey: process.env
         .APIKEY /* 请设置环境变量[APIKEY]以实现，你也可以在根目录下创建.env文件 */,
-    logo: "https://q2.a1pic.cn/2022/01/01/gtUy.jpg",
+    logo: "http://q1.qlogo.cn/g?b=qq&nk=3348626299&s=640",
     usePage: true,
 };
